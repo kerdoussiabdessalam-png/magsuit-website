05ec3c80d659fb54bc78e137b167846a324b2e14
kerdoussiabdessalam-png <kerdoussiabdessalam@gmail.com>
2025-11-27 14:12:23 -0500

    Create main.jsx

---
+++ src/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MagSuitLanding from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MagSuitLanding />
+  </React.StrictMode>,
+)
